e6ba39438820cff712075085dee925e64a794caf
Euan <bell_designs@outlook.com>
2018-04-16 16:18:33 +0100

    intial commit

---
+++ homework_A.rb
@@ -0,0 +1,22 @@
+class Student
+
+
+  def initialize (input_student_name, input_cohort)
+    @student_name = input_student_name
+    @input_cohort = input_cohort
+  end
+
+def get_student_name()
+  return @student_name
+end
+
+def get_input_cohort()
+  return @input_cohort
+end
+
+def set_student_name(new_name)
+  @student_name = new_name
+end
+
+
+end
---
+++ specs/homework_A_specs.rb
@@ -0,0 +1,28 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative('../homework_A.rb')
+
+class TestStudent < MiniTest::Test
+
+  def test_student_name
+    student = Student.new( "Euan", 21)
+    assert_equal('Euan', student.get_student_name())
+  end
+
+  def test_cohort
+    student = Student.new( "Euan", 21)
+    assert_equal(21, student.get_input_cohort())
+  end
+
+  def test_set_student_name
+    student = Student.new("Euan", 21)
+    student.set_student_name("Hamish")
+    assert_equal('Hamish', student.set_student_name())
+  end
+
+
+
+
+
+
+end
