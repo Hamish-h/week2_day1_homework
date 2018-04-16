bfe24d45de81ccac0b18a82a2e555395a1bee78b
Hanish-H <hamish.h@outlook.com>
2018-04-16 16:36:13 +0100

    changed paramtere

--- homework_A.rb
+++ homework_A.rb
@@ -1,22 +1,29 @@
 class Student
 
-
   def initialize (input_student_name, input_cohort)
     @student_name = input_student_name
     @input_cohort = input_cohort
   end
 
-def get_student_name()
-  return @student_name
-end
+  def get_student_name()
+    return @student_name
+  end
 
-def get_input_cohort()
-  return @input_cohort
-end
+  def get_input_cohort()
+    return @input_cohort
+  end
 
-def set_student_name(new_name)
-  @student_name = new_name
-end
+  def set_student_name(input_name)
+    @student_name = input_name
+  end
+
+
+
+
+
+
+
+
 
 
 end
--- specs/homework_A_specs.rb
+++ specs/homework_A_specs.rb
@@ -25,4 +25,6 @@ class TestStudent < MiniTest::Test
 
 
 
+
+
 end
